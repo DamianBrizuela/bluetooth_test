9e5943e1a5b2a662f0d0efe0e396c889f6e2927a
Damián Brizuela <dbrizuela@zoonsuite.com>
2025-11-03 16:47:37 -0300

    working code

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
-import bluetooth
+from bluetooth import DeviceDiscoverer, discover_devices, BluetoothError
+import logging
 
-print("Buscando dispositivos...")
-devices = bluetooth.discover_devices(lookup_names=True)
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logger = logging.getLogger()
 
-print("Dispositivos encontrados:")
-for addr, name in devices:
-    print(f"{name} - {addr}")
+def init_devices():
+
+    try:
+        logger.info("Buscando dispositivos...")
+        founded_devices = discover_devices(lookup_names=True)
+        if founded_devices:
+            for addr, name in founded_devices:
+                logger.info(f"Dispositivo encontrado: {name} - {addr}")
+    except BluetoothError as bt_err:
+        logger.error(f"Error Bluetooth al buscar dispositivos: {bt_err}")
+    except Exception as e:
+        logger.error(f"Error al buscar dispositivos: {e}")
